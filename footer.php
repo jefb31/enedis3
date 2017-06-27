e3223d9ffb382e540a0fec27ade8599bc681ea6b
quentin <zhor.osu@gmail.com>
2017-06-27 14:31:26 +0200

    initial commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="">
+        <p>&copy; G'eko 2017</p>
+        <p>Contact : </p>
+        <p>Credit : Lucas, Romain, Jef, Anne, Quentin</p>
+        <p>Mentions légales © Enedis 2016</p>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang=en>
+
+<head>
+    <meta charset=UTF-8>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./external/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Homepage</title>
+</head>
+<body>
+    <header>
+        <h1>G'eko</h1>
+        <nav class="navbar">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#"><img src="https://upload.wikimedia.org/wikipedia/fr/7/77/Logo_enedis_header.png" alt="logo" id="logo"></a>
+                    <button type="button" name="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu" aria-expanded="false">
+                <span class="sr-only"></span>
+                <span class="glyphicon glyphicon-menu-hamburger"></span>
+            </button>
+                </div>
+                <div class="collapse navbar-collapse" id="menu">
+                    <ul class="nav navbar-nav">
+                        <li><a href="home.php">Acceuil</a></li>
+                        <li><a href="saisons.php">Saisons</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
