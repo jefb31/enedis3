b56d919a0e6a104c2feb3f32a66f7eddfc66c21f
bonbers <guillot.romain@ymail.com>
2017-06-28 09:08:04 +0200

    Beta 1.0

--- script.js
+++ script.js
@@ -22,24 +22,64 @@ $("#summer").click(function(){
 '<div class="row menu2">'+
   '<div class="box boxp1 col-xs-12 col-sm-6">'+
     '<div id="p1">'+
-    '</div>'+
+      '<div class="rowcat row">'+
+        '<div class="imgcat col-xs-12 col-sm-12 col-md-4">'+
+        '<img class="img" src="img/clim.png" alt="clim">'+
+        '</div>'+
+        '<div class="txtcat1 col-xs-12 col-sm-12 col-md-8">'+
+          '<h3>Climatisation</h3>'+
+          '<p>bah i viur i iuiuv veivieviev ivbefibve iu ei</p>'+
+          '<p>bah i viur i iuiuv veivieviev ivbefibve iu ei</p>'+
+          '<p>bah i viur i iuiuv veivieviev ivbefibve iu ei</p>'+
+          '<p>bah i viur i iuiuv veivieviev ivbefibve iu ei</p>'+
+          '<p>bah i viur i iuiuv veivieviev ivbefibve iu ei</p>'+
+        '</div>'+
+        '</div>'+
+        '</div>'+
     '<div class="text1"></div>'+
   '</div>'+
 
   '<div class="box boxp2 col-xs-12 col-sm-6">'+
     '<div id="p2">'+
+    '<div class="rowcat row">'+
+      '<div class="imgcat col-xs-12 col-sm-12 col-md-4">'+
+      '<img class="img" src="img/techno.png" alt="electronique">'+
+      '</div>'+
+      '<div class="txtcat2 col-xs-12 col-sm-12 col-md-8">'+
+        '<h3>Electroménager / Multimédias</h3>'+
+        '<p>bah i viur i iuiuv veivieviev ivbefibve iu ei</p>'+
+      '</div>'+
+      '</div>'+
     '</div>'+
     '<div class="text2"></div>'+
   '</div>'+
 
   '<div class="box boxp3 col-xs-12 col-sm-6">'+
     '<div id="p3">'+
+    '<div class="rowcat row">'+
+      '<div class="imgcat col-xs-12 col-sm-12 col-md-4">'+
+      '<img class="img" src="img/piscine.jpg" alt="piscine">'+
+      '</div>'+
+      '<div class="txtcat3 col-xs-12 col-sm-12 col-md-8">'+
+        '<h3>Piscine</h3>'+
+        '<p>bah i viur i iuiuv veivieviev ivbefibve iu ei</p>'+
+      '</div>'+
+      '</div>'+
     '</div>'+
     '<div class="text3"></div>'+
   '</div>'+
 
   '<div class="box boxp4 col-xs-12 col-sm-6">'+
     '<div id="p4">'+
+    '<div class="rowcat row">'+
+      '<div class="imgcat col-xs-12 col-sm-12 col-md-4">'+
+      '<img class="img" src="img/light.png" alt="clim">'+
+      '</div>'+
+      '<div class="txtcat4 col-xs-12 col-sm-12 col-md-8">'+
+        '<h3>Autres Conseils</h3>'+
+        '<p>bah i viur i iuiuv veivieviev ivbefibve iu ei</p>'+
+      '</div>'+
+      '</div>'+
     '</div>'+
     '<div class="text4"></div>'+
   '</div>'+
@@ -67,24 +107,60 @@ $("#winter").click(function(){
 '<div class="row menu2">'+
   '<div class="box boxp5 col-xs-12 col-sm-6">'+
     '<div id="p5">'+
+    '<div class="rowcat row">'+
+      '<div class="imgcat col-xs-12 col-sm-12 col-md-4">'+
+      '<img class="img" src="img/chauffage.jpg" alt="clim">'+
+      '</div>'+
+      '<div class="txtcat1 col-xs-12 col-sm-12 col-md-8">'+
+        '<h3>Chauffage</h3>'+
+        '<p>bah i viur i iuiuv veivieviev ivbefibve iu ei</p>'+
+      '</div>'+
+      '</div>'+
     '</div>'+
     '<div class="text5"></div>'+
   '</div>'+
 
   '<div class="box boxp6 col-xs-12 col-sm-6">'+
     '<div id="p6">'+
+    '<div class="rowcat row">'+
+      '<div class="imgcat col-xs-12 col-sm-12 col-md-4">'+
+      '<img class="img" src="img/techno.png" alt="electronique">'+
+      '</div>'+
+      '<div class="txtcat2 col-xs-12 col-sm-12 col-md-8">'+
+        '<h3>Electroménager / Multimédias</h3>'+
+        '<p>bah i viur i iuiuv veivieviev ivbefibve iu ei</p>'+
+      '</div>'+
+      '</div>'+
     '</div>'+
     '<div class="text6"></div>'+
   '</div>'+
 
   '<div class="box boxp7 col-xs-12 col-sm-6">'+
     '<div id="p7">'+
+    '<div class="rowcat row">'+
+      '<div class="imgcat col-xs-12 col-sm-12 col-md-4">'+
+      '<img class="img" src="img/eau.jpg" alt="chauffe-eau">'+
+      '</div>'+
+      '<div class="txtcat3 col-xs-12 col-sm-12 col-md-8">'+
+        '<h3>Chauffe-eau</h3>'+
+        '<p>bah i viur i iuiuv veivieviev ivbefibve iu ei</p>'+
+      '</div>'+
+      '</div>'+
     '</div>'+
     '<div class="text7"></div>'+
   '</div>'+
 
   '<div class="box boxp8 col-xs-12 col-sm-6">'+
     '<div id="p8">'+
+    '<div class="rowcat row">'+
+      '<div class="imgcat col-xs-12 col-sm-12 col-md-4">'+
+      '<img class="img" src="img/light.png" alt="clim">'+
+      '</div>'+
+      '<div class="txtcat4 col-xs-12 col-sm-12 col-md-8">'+
+        '<h3>Autres conseils</h3>'+
+        '<p>bah i viur i iuiuv veivieviev ivbefibve iu ei</p>'+
+      '</div>'+
+      '</div>'+
     '</div>'+
     '<div class="text8"></div>'+
   '</div>'+
@@ -94,8 +170,7 @@ $("#winter").click(function(){
 });
 
 $("#profil").delegate("#p1", "click", function(){
-  $(".text1").toggle(1000);
-
+  $(".text1").toggle(500);
     $(".text1").html(
 
       '<p>conseil 1-1</p>'+
@@ -105,7 +180,7 @@ $("#profil").delegate("#p1", "click", function(){
   });
 
   $("#profil").delegate("#p2", "click", function(){
-    $(".text2").toggle(1000);
+    $(".text2").toggle(500);
       $(".text2").html(
 
         '<p>conseil 2-1</p>'+
@@ -115,7 +190,7 @@ $("#profil").delegate("#p1", "click", function(){
     });
 
     $("#profil").delegate("#p3", "click", function(){
-      $(".text3").toggle(1000);
+      $(".text3").toggle(500);
         $(".text3").html(
 
           '<p>conseil 3-1</p>'+
@@ -125,7 +200,7 @@ $("#profil").delegate("#p1", "click", function(){
       });
 
       $("#profil").delegate("#p4", "click", function(){
-        $(".text4").toggle(1000);
+        $(".text4").toggle(500);
           $(".text4").html(
 
             '<p>conseil 4-1</p>'+
@@ -135,7 +210,7 @@ $("#profil").delegate("#p1", "click", function(){
         });
 
         $("#profil").delegate("#p5", "click", function(){
-          $(".text5").toggle(1000);
+          $(".text5").toggle(500);
             $(".text5").html(
 
               '<p>conseil 5-1</p>'+
@@ -145,7 +220,7 @@ $("#profil").delegate("#p1", "click", function(){
           });
 
           $("#profil").delegate("#p6", "click", function(){
-            $(".text6").toggle(1000);
+            $(".text6").toggle(500);
               $(".text6").html(
 
                 '<p>conseil 6-1</p>'+
@@ -155,7 +230,7 @@ $("#profil").delegate("#p1", "click", function(){
             });
 
             $("#profil").delegate("#p7", "click", function(){
-              $(".text7").toggle(1000);
+              $(".text7").toggle(500);
                 $(".text7").html(
 
                   '<p>conseil 7-1</p>'+
@@ -165,7 +240,7 @@ $("#profil").delegate("#p1", "click", function(){
               });
 
               $("#profil").delegate("#p8", "click", function(){
-                $(".text8").toggle(1000);
+                $(".text8").toggle(500);
                   $(".text8").html(
 
                     '<p>conseil 8-1</p>'+
