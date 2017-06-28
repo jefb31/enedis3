3eedf3b1daef1a9e2c17d1560a8b6ce389ef86fb
bonbers <guillot.romain@ymail.com>
2017-06-28 09:52:51 +0200

    beta 1.0

--- index.php
+++ index.php
@@ -11,11 +11,16 @@
 
         <div class="boxsummer col-xs-6">
           <div id="summer">
+            <div class="bandeau">
+              <h4>Economiser l'été</h4>
+            </div>
           </div>
         </div>
 
         <div class="boxwinter col-xs-6">
           <div id="winter">
+            <div class="bandeau">
+              <h4>Economiser l'hiver</h4>
           </div>
         </div>
       </div>
